d7412f5a3995237691c93edc5c23adbdc78994e5
HillSJohnson <101838892+HillSJohnson@users.noreply.github.com>
2022-07-19 11:45:42 -0400

    Update files.py

--- python_sandbox_starter/files.py
+++ python_sandbox_starter/files.py
@@ -1,1 +1,19 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+#Open a file
+myFile = open('myfile.txt', 'w')
+
+#Get info
+print('Name: ', myFile.name)
+print('Is Closed: ', myFile.closed)
+print('Openign Mode: ', myFile.mode)
+
+#Write to file
+myFile.write('I love Python')
+myFile.write(' and JavaScript')
+myFile.close()
+
+#Append to file
+myFile = open('myfile.txt', 'a')
+myFile.write('I also like php')
+myFile.close()
