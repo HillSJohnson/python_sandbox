cf374e130384523d09e03b327452673860f47372
HillSJohnson <101838892+HillSJohnson@users.noreply.github.com>
2022-07-19 11:46:11 -0400

    Update variables.py

--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -1,4 +1,4 @@
-# A variable is a container for a value, which can be of various types
+#A variable is a container for a value, which can be of various types
 
 '''
 This is a 
@@ -13,3 +13,18 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+
+x = 1           #int
+y = 2.5         #float
+name = 'John'   #str
+is_cool = True  #boolean (has to have the capital T or F)
+
+#Mulitple Assignment
+x, y, name, is_cool = (1, 2.5, 'John', True)
+a = x+ y
+
+
+
+
+print(x)
+
